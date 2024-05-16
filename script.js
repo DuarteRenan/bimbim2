9177df1077f0f9c3caaf13d1baca1c8a29e23176
Renan Duarte <71858987+DuarteRenan@users.noreply.github.com>
2024-05-15 23:46:50 -0300

    Update scroll animation.js

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const navHeightPx = header.offsetHeight;
     const navHeightRem = navHeightPx / rootFontSize;
 
-    // Definir altura da navegação em unidades 'rem'
+ 
     document.documentElement.style.setProperty('--nav-height', `${navHeightRem}rem`);
 
-    // Adicionar evento de clique ao ícone do menu
     menuIcon.addEventListener('click', toggleMenu);
 
     function toggleMenu() {
@@ -28,6 +27,30 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
+// section animation
+
+    const sections = document.querySelectorAll('.wrapper');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+
+                entry.target.style.opacity = 1;
+                entry.target.style.transform = 'translateY(0rem)';
+            } else {
+
+                entry.target.style.opacity = 0;
+                entry.target.style.transform = 'translateY(var(--mb-xl))';
+            }
+        });
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+
+
     // Video
     const videoWrapper = document.querySelector('.video__wrapper')
     const videoPlayer = document.querySelector('#video-player')
@@ -38,41 +61,6 @@ document.addEventListener('DOMContentLoaded', function () {
         videoPlayer.controls = true;
         videoPlayer.setAttribute('autoplay', '');
     }
-
-    // Section animation
-    const sections = document.querySelectorAll('section');
-
-    sections.forEach(section => {
-        const children = section.querySelectorAll('*'); // Seleciona todos os elementos filhos da seção
-        children.forEach(child => {
-            child.setAttribute('data-screen-show', 'false'); // Define o valor inicial como false
-        });
-    });
-
-    function revealElements(entries) {
-        entries.forEach(entry => {
-            const isVisible = entry.isIntersecting; // Verifica se o elemento está visível na viewport
-            const children = entry.target.querySelectorAll('*'); // Seleciona todos os elementos filhos do elemento alvo
-            children.forEach(child => {
-                // Define o valor do atributo "data-screen-show" com base na visibilidade do elemento
-                child.setAttribute('data-screen-show', isVisible ? 'true' : 'false');
-            });
-        });
-    }
-
-    const observerOptions = {
-        root: null, // Usar o viewport como root
-        rootMargin: '0px', // Margem zero
-        threshold: 0.05 // Quando pelo menos metade do elemento está visível
-    };
-
-    const observer = new IntersectionObserver(revealElements, observerOptions);
-
-    // Iterar sobre cada seção
-    sections.forEach(section => {
-        // Observa cada seção
-        observer.observe(section);
-    });
 
     // Clientes animation
     const scrollers = document.querySelectorAll(".scroller-wrapper");
@@ -87,16 +75,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+
+    // remover # da URL
     const links = document.getElementsByTagName("a");
-    //Browse the previously created array
+   
     Array.prototype.forEach.call(links, function (elem) {
-        //Get the hyperlink target and if it refers to an id go inside condition
+        
         var elemAttr = elem.getAttribute("href");
         if (elemAttr && elemAttr.includes("#")) {
-            //Replace the regular action with a scrolling to target on click
+          
             elem.addEventListener("click", function (ev) {
                 ev.preventDefault();
-                //Scroll to the target element using replace() and regex to find the href's target id
+                
                 document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
                     behavior: "smooth",
                     block: "start",
